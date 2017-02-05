72c7788e95b2f16429d80c3f39498d2d5d15b44b
octacian <enduffy2014@outlook.com>
2017-02-04 16:14:47 -0800

    Basic groundwork (logger, load settings)

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ Debug Tools for Mod Developers [debugger]
 
 This is meant to be a simple tool for developers to use while attempting to debug issues in mods. See below for planned features. This README will be updated as features are added.
 
+Future tools and/or buttons are hidden from the creative inventory by default. To allow access in creative rather than just via `/giveme`, set `not_in_creative` to `0` in the `config.txt`.
+
 ### Planned Features
 * Meta/Inv Editor
 * Run Lua Code Under Environment (server won't crash)
--- init.lua
+++ init.lua
@@ -1,1 +1,22 @@
 -- debugger/init.lua
+
+debugger = {}
+
+debugger.modpath = minetest.get_modpath("debugger")
+local modpath = debugger.modpath
+
+-- Logger
+function debugger.log(content, log_type)
+  assert(content, "debugger.log: content nil")
+  if log_type == nil then log_type = "action" end
+  minetest.log(log_type, "[debugger] "..content)
+end
+
+-- Load Settings
+local Settings = Settings(modpath.."/config.txt"):to_table()
+
+if Settings then
+  debugger.CREATIVE = Settings.not_in_creative or 1
+else
+  debugger.CREATIVE = 1
+end
